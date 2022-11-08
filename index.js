652331a0f9d8f61e1a4774f2438ee386dd950091
noor16mustafa <c173212@ugrad.iiuc.ac.bd>
2022-11-08 11:10:20 +0600

    mongodb connected

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.erpq6o3.mongodb.net/?retryWrites=true&w=majority`;
+
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+
+app.get('/', (req, res) => {
+    res.send('Service review api is running');
+})
+
+app.listen(port, () => {
+    console.log(`Service Review Server is running on port ${port}`);
+})
