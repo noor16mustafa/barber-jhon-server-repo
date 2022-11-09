324e8aa2deb07a13c14cb5d21768413415311933
noor16mustafa <c173212@ugrad.iiuc.ac.bd>
2022-11-10 02:13:42 +0600

    review post

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -18,6 +18,7 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run() {
     try {
         const serviceCollection = client.db('barberServiceReview').collection('services');
+        const reviewCollection = client.db('barberServiceReview').collection('reviews');
 
         app.get('/servicesHome', async (req, res) => {
             const query = {};
@@ -31,6 +32,22 @@ async function run() {
             const cursor = serviceCollection.find(query);
             const services = await cursor.toArray();
             res.send(services);
+        });
+
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const service = await serviceCollection.findOne(query);
+
+            res.send(service);
+        });
+
+        //review api
+        app.post('/reviews', async (req, res) => {
+            const reviewDetails = req.body;
+            reviewDetails['time'] = new Date();
+            const result = await reviewCollection.insertOne(reviewDetails);
+            res.send(result);
         })
 
     }
